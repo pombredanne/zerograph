c46c4a4166725ac6503622254b6186ea7599ef18
Nigel Small <nigel@nigelsmall.com>
2014-02-23 19:34:36 +0000

    removed engines from environment

--- src/main/java/com/nigelsmall/zerograph/Environment.java
+++ src/main/java/com/nigelsmall/zerograph/Environment.java
@@ -14,21 +14,15 @@ public class Environment {
     final private ZMQ.Context context;
     final private String databaseHome;
     final private HashMap<String, GraphDatabaseService> databases;
-    final private HashMap<String, ExecutionEngine> engines;
 
     public Environment(String databaseHome) {
         this.context = ZMQ.context(1);
         this.databaseHome = databaseHome;
         this.databases = new HashMap<>();
-        this.engines = new HashMap<>();
     }
 
     public ZMQ.Context getContext() {
         return this.context;
-    }
-
-    public String getDatabaseHome() {
-        return this.databaseHome;
     }
 
     public synchronized GraphDatabaseService getDatabase(String name) {
@@ -38,17 +32,6 @@ public class Environment {
             GraphDatabaseService database = factory.newEmbeddedDatabase(databaseHome + "/" + name);
             databases.put(name, database);
             return database;
-        }
-    }
-
-    public synchronized ExecutionEngine getEngine(String name) {
-        GraphDatabaseService database = getDatabase(name);
-        if (engines.containsKey(name)) {
-            return engines.get(name);
-        } else {
-            ExecutionEngine engine = new ExecutionEngine(database);
-            engines.put(name, engine);
-            return engine;
         }
     }
 
