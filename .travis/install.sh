f28a0a8e721b8092c705798534efbb3ad8f96bd4
Nigel Small <nigel@nigelsmall.com>
2014-03-13 23:54:29 +0000

    simplified install

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,18 +1,4 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y python2.7 python3.3 python-virtualenv
-
-function python_install {
-    VENV=$1
-    PYTHON=$2
-    mkdir -p $VENV
-    virtualenv --python=$PYTHON $VENV
-    source $VENV/bin/activate
-    pip install -r requirements.txt
-    deactivate
-}
-
-cd src/client/python
-python_install venv/py27 `which python2.7`
-python_install venv/py33 `which python3.3`
+sudo pip install -r requirements.txt
 
