78acfb70736f1d5a8c56a03eb3da6e05ec57e756
Nigel Small <nigel@nigelsmall.com>
2014-04-23 20:57:40 +0100

    tweaked install.sh

--- src/travis/install.sh
+++ src/travis/install.sh
@@ -8,7 +8,9 @@ SRC=$HOME/src
 TEST=$SRC/test
 PYTHON=$SRC/client/python
 
-sudo apt-get install -y build-essentials
+sudo apt-get install -y build-essential
 
 cd $PYTHON
-sudo pip install -r requirements.txt
+sudo pip install -r $PYTHON/requirements.txt
+
+sudo pip install nose
